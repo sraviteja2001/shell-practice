75bdb741aa6263dc1fcf9df7e13f8b36d82aeef9
raviteja <info@joindevops.com>
2025-09-24 01:22:12 +0530

    02-variables.sh

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: hi $PERSON2, how are you?"
+echo "$PERSON2:: hello $PERSON1, iam fin. how you are?"
+echo "madi:: i am fine"
+echo "teja:: how r u"

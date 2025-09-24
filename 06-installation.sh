2a53608b417865dafafb251b9c0b2ac1d8a70b03
raviteja <info@joindevops.com>
2025-09-25 01:59:45 +0530

    04-conditions.sh

--- 06-installation.sh
+++ 06-installation.sh
@@ -11,6 +11,7 @@ dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: Installing MySQL is failure"
+    exit 1
 else
-    echo "Installing MySQL is SUCCESS"
+    echo "Installing MySQL is SUCCESS" 
 fi
